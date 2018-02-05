0d1b2cb213b3b4e2970e6a194957526bd3a599de
Lucas Baizer <titaniumsapphire32@gmail.com>
2018-02-04 20:00:12 -0800

    Work

--- src/cornflakes/lang/Main.java
+++ src/cornflakes/lang/Main.java
@@ -1,15 +1,17 @@
 package cornflakes.lang;
 
 import cornflakes.compiler.MainCompiler;
+import game.server.MainServer;
+import game.server.TestClient;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		MainCompiler.main(new String[] { "src" });
 
-		/*
-		 * Thread thread = new Thread(() -> { MainServer.main(new String[0]);
-		 * }); thread.start(); TestClient.main(new String[0]);
-		 */
-		// PointerTest.main(new String[0]);
+		Thread thread = new Thread(() -> {
+			MainServer.main(new String[0]);
+		});
+		thread.start();
+		TestClient.main(new String[0]);
 	}
 }
--- src/cornflakes/lang/Pointer.java
+++ src/cornflakes/lang/Pointer.java
@@ -8,7 +8,35 @@ public abstract class Pointer implements Serializable {
 	public static Pointer from(int val) {
 		return new I32Pointer(val);
 	}
-	
+
+	public static Pointer from(short val) {
+		return new I16Pointer(val);
+	}
+
+	public static Pointer from(long val) {
+		return new I64Pointer(val);
+	}
+
+	public static Pointer from(byte val) {
+		return new I8Pointer(val);
+	}
+
+	public static Pointer from(float val) {
+		return new F32Pointer(val);
+	}
+
+	public static Pointer from(double val) {
+		return new F64Pointer(val);
+	}
+
+	public static Pointer from(boolean val) {
+		return new BoolPointer(val);
+	}
+
+	public static Pointer from(char val) {
+		return new CharPointer(val);
+	}
+
 	public static Pointer from(Object val) {
 		return new ObjectPointer(val);
 	}
