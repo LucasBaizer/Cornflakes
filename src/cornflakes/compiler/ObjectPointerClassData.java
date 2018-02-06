2050336cc0137f43cf82b8f7f5c2a7d0812de60a
Lucas Baizer <titaniumsapphire32@gmail.com>
2018-02-05 21:08:54 -0800

    Removed some debug

--- src/cornflakes/compiler/ObjectPointerClassData.java
+++ src/cornflakes/compiler/ObjectPointerClassData.java
@@ -1,20 +1,16 @@
 package cornflakes.compiler;
 
-import java.util.Arrays;
-
 public class ObjectPointerClassData extends PointerClassData {
 	private String type;
-	
+
 	public ObjectPointerClassData(String type) {
 		super(type);
-		
+
 		this.type = type;
-		
-		System.out.println(type + " <- " + Arrays.asList(Thread.currentThread().getStackTrace()));
 
 		setClassName("cornflakes/lang/ObjectPointer");
 	}
-	
+
 	@Override
 	public DefinitiveType getValueType() {
 		return DefinitiveType.object(type);
